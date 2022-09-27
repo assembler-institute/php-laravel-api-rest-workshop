cfca32c63cfdda010ed732baeed9e609fd791879
Raúl López <hi@raullg.com>
2022-09-27 17:01:39 +0100

    workshop

---
+++ laravel-workshop/database/migrations/2022_09_27_155123_create_category_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class);
+            $table->foreignIdFor(Product::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+};
---
+++ laravel-workshop/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/categories', [CategoriesController::class, 'index']);
+Route::get('/categories/{category}', [CategoriesController::class, 'show']);
+Route::post('/categories', [CategoriesController::class, 'store']);
+Route::patch('/categories/{category}', [CategoriesController::class, 'update']);
+Route::delete('/categories/{category}', [CategoriesController::class, 'destroy']);
+
+Route::post('/products', [ProductsController::class, 'store']);
